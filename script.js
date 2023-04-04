619be56bfec1ea3efbd2f52c6e6dded8366c4d02
fatarick <61841951+fatarick@users.noreply.github.com>
2023-04-04 07:52:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const ai = document.getElementById("ai");
+
+ai.addEventListener("click", redirectToAI);
+
+function redirectToAI() {
+  window.location.href = "http://fatarick.pythonanywhere.com/";
+}
+
+const fos = document.getElementById("fos");
+
+fos.addEventListener("click", redirectToFOS);
+
+function redirectToFOS() {
+  window.location.href = "https://fatareos.netlify.app/";
+}
+
+const notch = document.getElementById("notchcustom");
+
+notch.addEventListener("click", redirectToNC);
+
+function redirectToNC() {
+  window.location.href = "https://fatarick.github.io/notchcustomiser/";
+}
+
+const wininf = document.getElementById("wininf");
+
+wininf.addEventListener("click", redirectToWI);
+
+function redirectToWI() {
+  window.location.href = "https://fataresite.netlify.app/";
+}
+
+const sbuild = document.getElementById("sbuild");
+
+sbuild.addEventListener("click", redirectToSB);
+
+function redirectToSB() {
+  window.location.href = "https://fatarick.github.io/2dskycrapersimulator/";
+}
+
+const discord = document.getElementById("discord");
+
+discord.addEventListener("click", redirectToDS);
+
+function redirectToDS() {
+  window.location.href = "https://discord.gg/RfmJQfu2yW";
+}
+
+const telega = document.getElementById("telega");
+
+telega.addEventListener("click", redirectToTG);
+
+function redirectToTG() {
+  window.location.href = "https://t.me/+_Awa7acaMnUzOTU0";
+}
+
+
